b620e23abdbb9f07bf42cbc52840a3c5dc44f6b6
AnsonAMS <anson_tohata@hotmail.com>
2021-02-08 14:20:52 +0100

    Add comonentDidMount() to fetch users

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-        robots: robots,
+        robots: [],
         searchfield: ''
     }
+
   }
+
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>  this.setState({ robots: users}));
+    
+}
 
   onSearchChange = (event) => {
       this.setState({ searchfield: event.target.value })
@@ -23,12 +31,15 @@ class App extends Component {
       const filteredRobots = this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-          <div className='tc'>
-            <h1 className='f2'>Robofriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filteredRobots} />
-          </div>
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else 
+          return (
+            <div className='tc'>
+              <h1 className='f2'>Robofriends</h1>
+              <SearchBox searchChange={this.onSearchChange} />
+              <CardList robots={filteredRobots} />
+            </div>
         );
     }
 }
